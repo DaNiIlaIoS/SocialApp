0591712e5a3bf5620860ec8bf2e7eeac8ee27ee1
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-07-15 12:10:01 +0500

    Create TabBarController

---
+++ SocialApp/Controller/MessagesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MessagesViewController.swift
+//  SocialApp
+//
+//  Created by Даниил Сивожелезов on 15.07.2024.
+//
+
+import UIKit
+import SnapKit
+
+final class MessagesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBlue
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
---
+++ SocialApp/Controller/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  SocialApp
+//
+//  Created by Даниил Сивожелезов on 15.07.2024.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .green
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
